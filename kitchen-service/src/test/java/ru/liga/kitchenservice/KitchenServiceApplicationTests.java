4e66e61e25a3e338ddbffe4a87e33e64ef70b0bf
destroY17 <eykheandrey@mail.ru>
2023-10-08 19:00:41 +0300

    add kitchen-service

---
+++ kitchen-service/src/test/java/ru/liga/kitchenservice/KitchenServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.liga.kitchenservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KitchenServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
