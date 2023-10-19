2b7d425f045d73b557592a5c209da7f3a04cbc75
destroY17 <eykheandrey@mail.ru>
2023-10-19 16:35:21 +0300

    fix: change insert data

--- migration/src/main/resources/db/changelog/1.0.1/insert-into-couriers.sql
+++ migration/src/main/resources/db/changelog/1.0.1/insert-into-couriers.sql
@@ -1,5 +1,5 @@
 insert into couriers (phone, status, coordinates)
 VALUES
-    ('123-456-7890', 'Active', '{"latitude": 40.7128, "longitude": -74.0060}'),
-    ('987-654-3210', 'Inactive', '{"latitude": 34.0522, "longitude": -118.2437}'),
-    ('555-555-5555', 'Active', '{"latitude": 51.5074, "longitude": -0.1278}');
+    ('123-456-7890', 'ACTIVE', '{"latitude": 40.7128, "longitude": -74.0060}'),
+    ('987-654-3210', 'INACTIVE', '{"latitude": 34.0522, "longitude": -118.2437}'),
+    ('555-555-5555', 'ACTIVE', '{"latitude": 51.5074, "longitude": -0.1278}');
--- migration/src/main/resources/db/changelog/1.0.1/insert-into-restaurants.sql
+++ migration/src/main/resources/db/changelog/1.0.1/insert-into-restaurants.sql
@@ -1,6 +1,6 @@
-INSERT INTO restaurants (address, status)
+INSERT INTO restaurants (name, address, status)
 VALUES
-    ('123 Main St, City, Country', 'Open'),
-    ('456 Elm St, Town, Country', 'Closed'),
-    ('789 Oak St, Village, Country', 'Open'),
-    ('101 Pine St, Beach, Country', 'Open');
+    ('Savory Delights Bistro', '123 Main St, City, Country', 'Open'),
+    ('Mango Fusion Grill', '456 Elm St, Town, Country', 'Closed'),
+    ('Cozy Corner Cafe', '789 Oak St, Village, Country', 'Open'),
+    ('Spice Island Kitchen', '101 Pine St, Beach, Country', 'Open');
