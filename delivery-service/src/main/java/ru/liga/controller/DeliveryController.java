6f083a08207c21c5bd675070910cfcc0fc689c19
destroY17 <eykheandrey@mail.ru>
2023-10-23 16:34:03 +0300

    feat: add new api method

--- delivery-service/src/main/java/ru/liga/controller/DeliveryController.java
+++ delivery-service/src/main/java/ru/liga/controller/DeliveryController.java
@@ -6,18 +6,25 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
 import ru.liga.dto.DeliveryDto;
+import ru.liga.model.Courier;
 import ru.liga.model.OrderStatus;
 import ru.liga.service.DeliveryService;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/deliveries")
 @AllArgsConstructor
 public class DeliveryController {
     private final DeliveryService deliveryService;
 
-    @GetMapping
+    @GetMapping("/deliveries")
     public Page<DeliveryDto> findAllDeliveries(@PageableDefault Pageable pageable,
                                                @RequestParam OrderStatus status) {
         return deliveryService.findDeliveriesByStatus(pageable, status);
+    }
+
+    @GetMapping("/couriers")
+    public List<Courier> findAllCouriers() {
+        return deliveryService.findAllCouriers();
     }
 }
--- delivery-service/src/main/java/ru/liga/service/impl/DeliveryServiceImpl.java
+++ delivery-service/src/main/java/ru/liga/service/impl/DeliveryServiceImpl.java
@@ -5,10 +5,13 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import ru.liga.batismapper.CourierMapper;
 import ru.liga.client.OrderFeign;
 import ru.liga.dto.DeliveryDto;
 import ru.liga.dto.OrderInfoDto;
+import ru.liga.exception.DataNotFoundException;
 import ru.liga.mapper.DeliveryToOrderMapper;
+import ru.liga.model.Courier;
 import ru.liga.model.OrderStatus;
 
 import ru.liga.service.DeliveryService;
@@ -20,6 +23,7 @@ import java.util.List;
 public class DeliveryServiceImpl implements DeliveryService {
     private final OrderFeign orderFeign;
     private final DeliveryToOrderMapper deliveryToOrderMapper;
+    private final CourierMapper courierMapper;
 
     @Override
     public Page<DeliveryDto> findDeliveriesByStatus(Pageable page, OrderStatus status) {
@@ -27,7 +31,13 @@ public class DeliveryServiceImpl implements DeliveryService {
         return new PageImpl<>(deliveryToOrderMapper.mapToDelivery(ordersByStatus.getContent()));
     }
 
-    public List<OrderInfoDto> findAllOrdersByStatus(OrderStatus status) {
-        return orderFeign.findOrdersByStatus(status).getContent();
+    public List<Courier> findAllCouriers() {
+        return courierMapper.findAll();
+    }
+
+    public Courier findCourierById(Long id) {
+        return courierMapper.findCourierById(id)
+                .orElseThrow(() ->
+                        new DataNotFoundException("Courier with id = " + id + " is not found"));
     }
 }
