a40353aeebfdbf07083fc56d68a1dce605f01be0
destroY17 <eykheandrey@mail.ru>
2023-10-26 18:13:57 +0300

    fix: change status to correctly

--- migration/src/main/resources/db/changelog/1.0.1/insert-into-orders.sql
+++ migration/src/main/resources/db/changelog/1.0.1/insert-into-orders.sql
@@ -1,6 +1,6 @@
 INSERT INTO orders (customer_id, restaurant_id, status, courier_id, timestamp)
 VALUES
-    (1, 1, 'PENDING', null, '2023-10-15 12:00:00'),
-    (2, 2, 'PICKING', 1, '2023-10-15 13:30:00'),
-    (3, 3, 'DELIVERING', 3, '2023-10-15 14:45:00'),
-    (1, 2, 'COMPLETE', 2, '2023-10-15 15:15:00');
+    (1, 1, 'KITCHEN_ACCEPTED', null, '2023-10-15 12:00:00'),
+    (2, 2, 'DELIVERY_PICKING', 1, '2023-10-15 13:30:00'),
+    (3, 3, 'DELIVERY_DELIVERING', 3, '2023-10-15 14:45:00'),
+    (1, 2, 'DELIVERY_COMPLETE', 2, '2023-10-15 15:15:00');
